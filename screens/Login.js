74885b499eedee265e334dd878993db0ea8cb164
Abdullah Emad <abdullahem1997@hotmail.com>
2018-07-08 16:28:07 +0200

    Create Register and login screens

--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,70 @@
 import React from "react";
-import { Button, View, StyleSheet, Text, AsyncStorage } from "react-native";
+import { Button, View, StyleSheet, Text, AsyncStorage, ImageBackground, TextInput} from "react-native";
 
-export default class Login extends React.Component{
-	
+export default class Register extends React.Component{
+
 	render(){
 		return (
-			<View>
-				<Text>This is the login page and its not supposed to be displayed now</Text>
+			<View style={styles.parent}> 
+				<ImageBackground
+		    	  style={styles.backgroundImage}
+		     	  source={require('./../assets/background.jpg')}
+		   		>
+					<View style={styles.button}>
+						<TextInput
+						  placeholder="Email"
+						  style={styles.textbox}
+						/>
+						<TextInput
+						  placeholder="Password"
+						  style={styles.textbox}
+						  secureTextEntry={true}
+						/>
+					</View>
+					<View style={styles.button}>
+						<Button title = "Register" onPress = {this.onRegister}/>
+					</View>
+				</ImageBackground>
 			</View>
 		);
 	}
+
+	onLogin = () =>
+	{
+		this.props.navigation.navigate("Login");
+	};
+
+	onRegister = () =>
+	{
+		this.props.navigation.navigate("Register");
+	};
+
 }
+
+
+const styles = StyleSheet.create({
+	parent: 
+	{
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+        alignItems: 'center',
+	
+	},
+	backgroundImage: {
+		width: '100%',
+		height: '100%',
+		flexDirection: 'column',
+		justifyContent: 'center',
+        alignItems: 'center',
+	},
+	textbox: {
+		backgroundColor: 'white',
+		fontSize: 20,
+		paddingBottom:'1%',
+	},
+	button: {
+		width: '50%',
+		paddingBottom: '1%',
+	},
+});
--- screens/Register.js
+++ screens/Register.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, View, StyleSheet, Text, AsyncStorage, ImageBackground } from "react-native";
+import { Button, View, StyleSheet, Text, AsyncStorage, ImageBackground, TextInput} from "react-native";
 
-export default class Register extends React.Component{
-
+export default class Login extends React.Component{
+	
 	render(){
 		return (
 			<View style={styles.parent}> 
@@ -11,7 +11,24 @@ export default class Register extends React.Component{
 		     	  source={require('./../assets/background.jpg')}
 		   		>
 					<View style={styles.button}>
-						<Button title = "Login" onPress = {this.onLogin}/>
+						<TextInput
+						  placeholder="Name"
+						  style={styles.textbox}
+						/>
+						<TextInput
+						  placeholder="Email"
+						  style={styles.textbox}
+						/>
+						<TextInput
+						  placeholder="Password"
+						  style={styles.textbox}
+						  secureTextEntry={true}
+						/>
+						<TextInput
+						  placeholder="Confirm Password"
+						  style={styles.textbox}
+						  secureTextEntry={true}
+						/>
 					</View>
 					<View style={styles.button}>
 						<Button title = "Register" onPress = {this.onRegister}/>
@@ -30,8 +47,9 @@ export default class Register extends React.Component{
 	{
 		this.props.navigation.navigate("Register");
 	};
-
 }
+
+
 
 
 const styles = StyleSheet.create({
@@ -49,6 +67,11 @@ const styles = StyleSheet.create({
 		flexDirection: 'column',
 		justifyContent: 'center',
         alignItems: 'center',
+	},
+	textbox: {
+		backgroundColor: 'white',
+		fontSize: 20,
+		paddingBottom:'1%',
 	},
 	button: {
 		width: '50%',
